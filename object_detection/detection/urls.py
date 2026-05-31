716e556ad7a503c69ae40a975e55e49c92598389
Maryam Fatima <maryamzakir0323@gmail.com>
2026-05-31 18:34:51 +0500

    Delete object_detection directory

--- object_detection/detection/urls.py
+++
@@ -1,8 +0,0 @@
-
-from django.urls import path
-from .views import ObjectDetectionView
-
-urlpatterns = [
-    path('upload/', ObjectDetectionView.as_view(), name='upload_image'),
-    # other paths for detection app
-]
